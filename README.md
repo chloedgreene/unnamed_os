0acae74d419b4176ff39ba964e526079cfb80ecd
Chloe Greene <thecatliferules@gmail.com>
2023-11-13 16:06:13 -0500

    setup vscode settings

--- README.md
+++ README.md
@@ -5,7 +5,11 @@
 
 ### Sources:
 https://wiki.osdev.org/Main_Page
+
 http://createyourownos.blogspot.com/
+
 http://www.dtweed.com/circuitcellar/
+
 https://littleosbook.github.io/
+
 https://csc-knu.github.io/sys-prog/books/Andrew%20S.%20Tanenbaum%20-%20Operating%20Systems.%20Design%20and%20Implementation.pdf
---
+++ k/ccheck.h
@@ -0,0 +1,11 @@
+
+// When were compiling to a bare metal platform, we need to check if the compiler toolchain is working correctly
+
+#if defined(__linux__)
+#error "You are not using a cross-compiler, you will most certainly run into trouble"
+#endif
+ 
+/* This tutorial will only work for the 32-bit ix86 targets. */
+#if !defined(__i386__)
+#error "This tutorial needs to be compiled with a ix86-elf compiler"
+#endif
